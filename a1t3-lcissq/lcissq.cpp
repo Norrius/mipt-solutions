4618dcc71f2fe755f53f9815f051dd976893fbc9
Norrius <norrius@gmail.com>
2014-03-17 14:09:47 +0400

    N*2 memory instead of N^2.

--- a1t3-lcissq/lcissq.cpp
+++ a1t3-lcissq/lcissq.cpp
@@ -5,7 +5,7 @@ int lcissq(const std::vector<int> &a, const std::vector<int> &b) {
     int m = b.size();
 
     std::vector<std::vector<int> > d(n);
-    for (int i=0; i<n; ++i) {
+    for (int i=0; i<2; ++i) {
         d[i] = std::vector<int>(m);
     }
 
@@ -14,14 +14,14 @@ int lcissq(const std::vector<int> &a, const std::vector<int> &b) {
     for (int i=0; i<n; ++i) {
         best = 0;
         for (int j=0; j<m; ++j) {
-            d[i][j] = (i>0?d[i-1][j]:0);
-            if (a[i] == b[j] && d[i][j] <= best) {
-                d[i][j] = best + 1;
-            } else if (a[i] > b[j] && d[i][j] > best) {
-                best = d[i][j];
+            d[i%2][j] = (i>0?d[(i-1)%2][j]:0);
+            if (a[i] == b[j] && d[i%2][j] <= best) {
+                d[i%2][j] = best + 1;
+            } else if (a[i] > b[j] && d[i%2][j] > best) {
+                best = d[i%2][j];
             }
         }
     }
 
-    return *(std::max_element(d[n-1].begin(), d[n-1].end()));
+    return *(std::max_element(d[(n-1)%2].begin(), d[(n-1)%2].end()));
 }
