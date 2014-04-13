cc07289e6aabb3a7df8178835136052d494a6a68
Norrius <norrius@gmail.com>
2014-04-13 16:02:31 +0400

    Strongly connected subgraphs (Kosaraju).

---
+++ a2t3-strongly-connected/graph.cpp
@@ -0,0 +1,28 @@
+#include "graph.h"
+
+Graph::Graph(size_t size) :
+    arcs_(size)
+{}
+
+Graph::Graph(const Graph &other) :
+    arcs_(other.arcs_)
+{}
+
+void Graph::addArc(size_t a, size_t b)
+{
+    if (std::max(a,b)+1 > arcs_.size()) {
+        arcs_.resize(std::max(a,b)+1);
+    }
+    arcs_[a].push_back(b);
+}
+
+Graph Graph::getInverted()
+{
+    Graph inverted(arcs_.size());
+    for (std::vector<std::list<size_t> >::iterator it = arcs_.begin(); it != arcs_.end(); ++it) {
+        for (std::list<size_t>::iterator jt = it->begin(); jt != it->end(); ++jt) {
+            inverted.arcs_[*jt].push_back(it-arcs_.begin());
+        }
+    }
+    return inverted;
+}
